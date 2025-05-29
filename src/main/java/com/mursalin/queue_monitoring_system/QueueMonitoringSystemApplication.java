bbbf5b79ae7c7cc05692aa287c63c55ba3d485d1
mohammad-mursalin <mursalinlamon@gmail.com>
2025-05-29 09:20:27 +0600

    first commit

---
+++ src/main/java/com/mursalin/queue_monitoring_system/QueueMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mursalin.queue_monitoring_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueMonitoringSystemApplication.class, args);
+	}
+
+}
