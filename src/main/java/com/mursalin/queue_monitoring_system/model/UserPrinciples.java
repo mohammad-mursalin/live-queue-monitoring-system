5ac2940f8c9fde8e2daaa834ac0b18deff0df7db
mohammad-mursalin <mursalinlamon@gmail.com>
2025-05-30 15:37:07 +0600

    user details class

---
+++ src/main/java/com/mursalin/queue_monitoring_system/model/UserPrinciples.java
@@ -0,0 +1,57 @@
+package com.mursalin.queue_monitoring_system.model;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class UserPrinciples implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(user.getRole().toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getId() {
+        return user.getId();
+    }
+}
