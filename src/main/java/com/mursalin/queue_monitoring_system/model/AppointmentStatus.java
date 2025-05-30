dbfbc69b168351aa15960acf1777e78d5295b393
mohammad-mursalin <mursalinlamon@gmail.com>
2025-05-30 15:35:28 +0600

    enums

---
+++ src/main/java/com/mursalin/queue_monitoring_system/model/AppointmentStatus.java
@@ -0,0 +1,5 @@
+package com.mursalin.queue_monitoring_system.model;
+
+public enum AppointmentStatus {
+    WAITING, IN_SESSION, COMPLETED, SCHEDULED, CANCELLED
+}
---
+++ src/main/java/com/mursalin/queue_monitoring_system/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.mursalin.queue_monitoring_system.model;
+
+public enum UserRole {
+    ROLE_USER,
+    ROLE_PATIENT,
+    ROLE_ADMIN
+}
