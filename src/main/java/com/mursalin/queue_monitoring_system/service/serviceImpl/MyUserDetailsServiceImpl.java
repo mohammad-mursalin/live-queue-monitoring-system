4ffdba645d1e51e395fdc2e932589a08ce96c328
mohammad-mursalin <mursalinlamon@gmail.com>
2025-06-16 12:15:18 +0600

    UserDetailsService implementation

--- src/main/java/com/mursalin/queue_monitoring_system/service/serviceImpl/MyUserDetailsServiceImpl.java
+++ src/main/java/com/mursalin/queue_monitoring_system/service/serviceImpl/MyUserDetailsServiceImpl.java
@@ -14,7 +14,18 @@ import org.springframework.stereotype.Service;
 public class MyUserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
-    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        try {
+            User user = userRepository.findByEmail(email).orElseThrow(()-> new RuntimeException("User not found"));
+
+            return new UserPrinciples(user);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public UserDetails loadUserById(String id) throws UsernameNotFoundException {
         try {
             User user = userRepository.findById(id).orElseThrow(()-> new RuntimeException("User not found"));
 
