cca881b1d178d5687d990ccdeb5fcc89b7a54ee4
mohammad-mursalin <mursalinlamon@gmail.com>
2025-06-16 10:17:31 +0600

    removed deprecated method to set the user details service

--- src/main/java/com/mursalin/queue_monitoring_system/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/mursalin/queue_monitoring_system/service/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mursalin.queue_monitoring_system.service.serviceImpl;
 
 import com.mursalin.queue_monitoring_system.dto.UserDto;
+import com.mursalin.queue_monitoring_system.jwt.JwtService;
 import com.mursalin.queue_monitoring_system.model.User;
 import com.mursalin.queue_monitoring_system.model.UserRole;
 import com.mursalin.queue_monitoring_system.repository.UserRepository;
@@ -9,6 +10,12 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +23,9 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     @Override
     public ResponseEntity<?> registerUser(@NonNull UserDto userDto) {
@@ -24,6 +34,7 @@ public class UserServiceImpl implements UserService {
                     .name(userDto.getName())
                     .phone(userDto.getPhone())
                     .email(userDto.getEmail())
+                    .password(encoder.encode(userDto.getPassword()))
                     .role(UserRole.ROLE_USER)
                     .build();
             User savedUser = userRepository.save(user);
@@ -34,6 +45,23 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseEntity userLogin(@NonNull UserDto userDto) {
-        return null;
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(userDto.getEmail(), userDto.getPassword()));
+
+            User authenticatedUser = userRepository.findByEmailAndPhoneIgnoreCase(userDto.getEmail())
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+
+            if (authentication.isAuthenticated()) {
+                String jwt = jwtService.generateToken(authenticatedUser);
+                return new ResponseEntity<>(jwt, HttpStatus.OK);
+            }
+            throw new RuntimeException("Login failed due to invalid credentials.");
+
+
+        } catch (AuthenticationException ex) {
+            throw new RuntimeException("Invalid email or password.");
+        }
     }
 }
