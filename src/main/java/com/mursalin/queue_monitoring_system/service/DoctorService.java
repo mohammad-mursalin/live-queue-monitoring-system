f97521405414cc2198ab43784d3095094aa855bc
mohammad-mursalin <mursalinlamon@gmail.com>
2025-06-16 12:14:23 +0600

    service interface

--- src/main/java/com/mursalin/queue_monitoring_system/service/DoctorService.java
+++ src/main/java/com/mursalin/queue_monitoring_system/service/DoctorService.java
@@ -1,11 +1,14 @@
 package com.mursalin.queue_monitoring_system.service;
 
+import com.mursalin.queue_monitoring_system.dto.DoctorDto;
 import com.mursalin.queue_monitoring_system.model.Doctor;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface DoctorService {
-    ResponseEntity<?> getAllDoctors();
+    ResponseEntity<List<DoctorDto>> getAllDoctors();
 
     ResponseEntity<?> addDoctor(@NotNull Doctor doctor);
 }
