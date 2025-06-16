fe7b6fe0003b0c5f2d9ba53c4cf063f701622f05
mohammad-mursalin <mursalinlamon@gmail.com>
2025-06-16 10:19:12 +0600

    new methods added

--- src/main/java/com/mursalin/queue_monitoring_system/repository/UserRepository.java
+++ src/main/java/com/mursalin/queue_monitoring_system/repository/UserRepository.java
@@ -3,6 +3,12 @@ package com.mursalin.queue_monitoring_system.repository;
 import com.mursalin.queue_monitoring_system.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String > {
     boolean existByEmailAndPhoneIgnoreCase(String email, String phone);
+
+    Optional<User> findByEmailAndPhoneIgnoreCase(String email);
+
+    Optional<User> findByEmail(String email);
 }
