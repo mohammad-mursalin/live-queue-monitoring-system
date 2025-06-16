81200b3b9428c0fbc8218c48a7af2d90024ca0bf
mohammad-mursalin <mursalinlamon@gmail.com>
2025-06-16 12:13:04 +0600

    fetching, removing and updating doctor

--- src/main/java/com/mursalin/queue_monitoring_system/controller/DoctorController.java
+++ src/main/java/com/mursalin/queue_monitoring_system/controller/DoctorController.java
@@ -21,4 +21,21 @@ public class DoctorController {
     public ResponseEntity<?> addDoctor(@RequestBody @NotNull Doctor doctor) {
         return doctorService.addDoctor(doctor);
     }
+
+    @GetMapping("/doctor-list")
+    public ResponseEntity<List<DoctorDto>> getDoctorList() {
+        return doctorService.getAllDoctors();
+    }
+
+    @PutMapping("/update-doctor")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<DoctorDto> updateDoctor(@RequestBody DoctorDto doctorDto) {
+        return doctorService.updateDoctor(doctorDto);
+    }
+
+    @DeleteMapping("/remove-doctor/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteDoctor(@PathVariable String id) {
+        return doctorService.removeDoctor(id);
+    }
 }
